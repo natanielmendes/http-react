5f8f11478ff245a7bb27959d40cc11f29a803568
Nataniel Carvalho <natanielmendes@hotmail.com>
2020-08-21 18:20:02 -0300

    Handling Errors Locally

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -10,7 +10,8 @@ class Blog extends Component {
 
     state = {
         posts: [],
-        selectedPostId: null
+        selectedPostId: null,
+        error: false
     }
 
     componentDidMount() {
@@ -25,6 +26,10 @@ class Blog extends Component {
                 });
                 this.setState({posts: updatedPosts});
             })
+            .catch(error => {
+                this.setState({error: true})
+                console.log(error)
+            })
     }
 
     postSelectedHandler = (id) => {
@@ -32,13 +37,16 @@ class Blog extends Component {
     }
 
     render () {
-        const posts = this.state.posts.map(post => {
-            return <Post
-                key={post.id}
-                title={post.title}
-                author={post.author}
-                clicked={() => this.postSelectedHandler(post.id)} />
-        });
+        let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return <Post
+                    key={post.id}
+                    title={post.title}
+                    author={post.author}
+                    clicked={() => this.postSelectedHandler(post.id)} />
+            });
+        }
 
         return (
             <div>
