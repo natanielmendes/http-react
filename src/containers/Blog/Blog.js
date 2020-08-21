850224a18c7673b8be6bbd86e4dc63678a6f4fc4
Nataniel Carvalho <natanielmendes@hotmail.com>
2020-08-20 23:33:08 -0300

    Transforming Data

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -15,13 +15,20 @@ class Blog extends Component {
     componentDidMount() {
         axios.get('https://jsonplaceholder.typicode.com/posts')
             .then(response => {
-                this.setState({posts: response.data});
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Natan'
+                    }
+                });
+                this.setState({posts: updatedPosts});
             })
     }
 
     render () {
         const posts = this.state.posts.map(post => {
-            return <Post title={post.title} key={post.id} />
+            return <Post key={post.id} title={post.title} author={post.author} />
         });
 
         return (
